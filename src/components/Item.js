ccd953af70c1edecec3f84d6e852ce3d878f10b6
Guido <46787485+guidoFortunato@users.noreply.github.com>
2022-08-15 20:28:11 -0300

    mejora del footer y agrego modal

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,43 +1,42 @@
-import React from 'react'
-import { Card, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
+import React from "react";
+import { Card, Col, ListGroup, ListGroupItem } from "react-bootstrap";
 //import { useNavigate } from 'react-router-dom';
-import { Button } from '@chakra-ui/react'
-import { alertConfirmationSuccess } from '../alerts/alerts';
-import { useNavigate } from 'react-router-dom';
+import { Button } from "@chakra-ui/react";
+import { alertConfirmationSuccess } from "../alerts/alerts";
+import { useNavigate } from "react-router-dom";
 
-import '../index.css'
+import "../index.css";
 const Item = ({ products }) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handlePurchase = () => {
+    navigate("/productos/metodo-pago");
+    alertConfirmationSuccess();
+  };
 
-    const handlePurchase = ()=>{
-        navigate('/productos/metodo-pago')
-        alertConfirmationSuccess()
-    }
+  return (
+    <Col>
+      <Card border="success" className="personajes-principal card-detail">
+        <Card.Img variant="top" src={products.imagen} />
+        <Card.Body>
+          <Card.Title className="personaje-titulo">
+            {products.nombre}
+          </Card.Title>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Descripcion</ListGroupItem>
+            <ListGroupItem>Precio en pesos: ${products.precio}</ListGroupItem>
+          </ListGroup>
+          <Button
+            colorScheme="purple"
+            className="addButton"
+            onClick={() => handlePurchase()}
+          >
+            Comprar
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
 
-
-    return (
-
-        <Col >
-            <Card border="success" className="personajes-principal card-detail"  >
-                <Card.Img variant="top" src={products.imagen} />
-                <Card.Body>
-                    <Card.Title className="personaje-titulo">{products.nombre}</Card.Title>
-                    <ListGroup className="list-group-flush">
-                        <ListGroupItem >Descripcion</ListGroupItem>
-                        <ListGroupItem >Precio en pesos: ${products.precio}</ListGroupItem>
-
-
-                    </ListGroup>
-                    <Button colorScheme='purple' className="addButton" onClick={()=> handlePurchase()}>Comprar</Button>
-
-                </Card.Body>
-
-            </Card>
-        </Col>
-
-
-    )
-}
-
-export default Item
+export default Item;
