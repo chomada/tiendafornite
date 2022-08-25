d205b52fec0832cac2ce22f951cabcc4ff54a27f
AgostinoNicolas <nico.agostino@hotmail.com>
2022-08-25 00:09:48 -0300

    arreglo en metodos de pagos

--- src/components/PaymentMethods.jsx
+++ src/components/PaymentMethods.jsx
@@ -1,4 +1,3 @@
-import { Container, Row, Col} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import MercadoPago from "../assets/mercadoPago.png";
 import TransferenciaBancaria from "../assets/transferencia-bancaria.jpg";
@@ -9,47 +8,42 @@ export const PaymentMethods = () => {
   const redirigir = () => {
     navigate('/transferencia-bancaria')
   }
-  document.body.style = 'background: linear-gradient(black, #472c4a 60%, #472c4a) !important';
+
   return (
     <>
-      <Container>
-        <Row>
-          <Col>
-                <h2 className="contenedor-metodosPagos">Métodos de Pago</h2>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="contenedor-pago bg-dark">
-              <div className="d-flex justify-content-center">
-                <div className="d-flex justify-content-center">
-                  <img
-                      src={ TransferenciaBancaria }
-                      alt="mercado pago"
-                      className="img-thumbnail w-75 p-0 pointer"
-                      onClick={()=> redirigir()}
-                    />
-                </div>
-                <div className="d-flex justify-content-center">
-                  <img
-                    src={ MercadoPago }
-                    alt="mercado pago"
-                    className="img-thumbnail w-75 p-0 pointer"
-                  />
-                  </div>
-              </div>             
-              <div className="col-12 text-center mt-5">
-                <button
-                  className="btn btn-primary text-center mt-5"
-                  onClick={() => navigate(-1)}
-                >
-                  Volver
-                </button>
-              </div>
-            </div>     
-          </Col>
-        </Row>
-      </Container>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="d-flex justify-content-center flex-column align-items-center mb-4">
+              <h2 className="contenedor-metodosPagos">Métodos de Pago</h2>
+            </div>
+          </div>
+         
+          <div className="col-12 col-lg-6 d-flex justify-content-center animate__animated animate__fadeIn mb-5 mb-lg-0">
+          <img
+              src={ TransferenciaBancaria }
+              alt="mercado pago"
+              className="img-thumbnail w-75 p-0 pointer"
+              onClick={()=> redirigir()}
+            />
+          </div>
+          <div className="col-12 col-lg-6 d-flex justify-content-center animate__animated animate__fadeIn">
+            <img
+              src={ MercadoPago }
+              alt="mercado pago"
+              className="img-thumbnail w-75 p-0 pointer"
+            />
+          </div>
+          <div className="col-12 text-center mt-5">
+            <button
+            className="btn btn-primary text-center mt-5"
+            onClick={() => navigate(-1)}
+            >
+            Volver
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
