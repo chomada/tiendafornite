4b5b6afc98c6c5030d88405528b505d980da46a0
AgostinoNicolas <nico.agostino@hotmail.com>
2022-08-24 23:21:04 -0300

    cambios en el css

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,43 @@
+import { Col, Container, Row } from "react-bootstrap";
+import ComoComprar from "../ComoComprar";
+import Header from "../Header";
+
+const Home = () => {
+    return(
+        <>
+            <Header/>
+            <Container>
+                <Row>
+                    <Col className="contenedor-productos">
+                        <h2>Productos</h2> 
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className="contenedor-gesto">
+                            <div className="titulo-gesto bg-dark">
+                                <h3>Gestos</h3>
+                            </div>
+                            <div>
+
+
+                            </div>
+
+                        </div>
+                    </Col>
+                </Row>         
+            </Container>
+            <Container fluid>
+                <Row>
+                    <Col className="parallax">
+                    </Col>
+                </Row>
+            </Container>
+            <ComoComprar/>   
+        </>
+    );
+    
+}
+
+
+export default Home;
