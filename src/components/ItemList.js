78526ab4202101714329f6818579ef4fcdff8941
Guido <46787485+guidoFortunato@users.noreply.github.com>
2022-08-29 12:39:02 -0300

    saco productos de pagina principal

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,7 +6,7 @@ import { Row } from "react-bootstrap";
 const ItemList = ({ products }) => {
  
   return (
-    <Row xs={2} sm={2} lg={3} xl={4} className="g-4 personajes-principal">
+    <Row xs={1} sm={2} lg={3} xl={4} className="g-4 personajes-principal">
       {products.map((prod) => {
         return <Item key={prod.id} products={prod} />;
       })}
