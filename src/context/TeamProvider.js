c5021f5c619ddad9be464924553eb8e0f67ea490
Federico Machado <fmachado@interbanking.com.ar>
2022-09-06 18:54:00 -0300

    localstorage se agrega

--- src/context/TeamProvider.js
+++
@@ -1,9 +0,0 @@
-import React, { createContext } from "react";
-
-export const Team = createContext();
-
-const TeamProvider = ({ children }) => {
-  return <Team.Provider value={{}}>{children}</Team.Provider>;
-};
-
-export default TeamProvider;
